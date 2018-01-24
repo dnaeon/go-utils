007a9d615a7f42ab8d764cb2b3cbb703ed8ad27c
Marin Atanasov Nikolov <mnikolov@vmware.com>
2018-01-24 08:44:02 +0200

    Initial commit of `map` and `slice` packages
    The map and slice packages have been imported from the
    github.com/dnaeon/gru/utils package.

---
+++ map/map.go
@@ -0,0 +1,88 @@
+// Copyright (c) 2015-2018 Marin Atanasov Nikolov <dnaeon@gmail.com>
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer
+//     in this position and unchanged.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package utils
+
+import "sync"
+
+// ConcurrentMap is a map type that can be safely shared between
+// goroutines that require read/write access to a map.
+type ConcurrentMap struct {
+	sync.RWMutex
+	items map[string]interface{}
+}
+
+// ConcurrentMapItem contains a key/value pair item of a concurrent map.
+type ConcurrentMapItem struct {
+	Key   string
+	Value interface{}
+}
+
+// NewConcurrentMap creates a new concurrent map.
+func NewConcurrentMap() *ConcurrentMap {
+	cm := &ConcurrentMap{
+		items: make(map[string]interface{}),
+	}
+
+	return cm
+}
+
+// Set adds an item to a concurrent map.
+func (cm *ConcurrentMap) Set(key string, value interface{}) {
+	cm.Lock()
+	defer cm.Unlock()
+
+	cm.items[key] = value
+}
+
+// Get retrieves the value for a concurrent map item.
+func (cm *ConcurrentMap) Get(key string) (interface{}, bool) {
+	cm.Lock()
+	defer cm.Unlock()
+
+	value, ok := cm.items[key]
+
+	return value, ok
+}
+
+// Iter iterates over the items in a concurrent map.
+// Each item is sent over a channel, so that
+// we can iterate over the map using the builtin range keyword.
+func (cm *ConcurrentMap) Iter() <-chan ConcurrentMapItem {
+	c := make(chan ConcurrentMapItem)
+
+	f := func() {
+		cm.Lock()
+		defer cm.Unlock()
+
+		for k, v := range cm.items {
+			c <- ConcurrentMapItem{k, v}
+		}
+		close(c)
+	}
+	go f()
+
+	return c
+}
